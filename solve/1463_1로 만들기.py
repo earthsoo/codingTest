0d9c9d59f139b9778ffc7e39ff82db6f725c5373
JISOO KANG <128710723+earthsoo@users.noreply.github.com>
2026-01-10 15:37:40 +0900

    Update 1463_1로 만들기.py

--- solve/1463_1로 만들기.py
+++ solve/1463_1로 만들기.py
@@ -10,7 +10,9 @@ n = int(input())
 cnt=[0]*(n+1)
 #2부터 n까지 반복
 for i in range(2, n+1):
+  cnt[i]=cnt[i-1]+1
   if i % 3 == 0:
-    cnt[i] = min(cnt[i-1]+1, cnt[(i//3)+1])
+    cnt[i] = min(cnt[i], cnt[i//3]+1)
   if i % 2 == 0:
-    cnt[i] = min(cnt[i-1]+1, cnt[(i//2)+1])
+    cnt[i] = min(cnt[i], cnt[i//2]+1)
+print(cnt[i])
