f25288d02f83a59a1115a737906897b138c37351
JISOO KANG <128710723+earthsoo@users.noreply.github.com>
2026-02-13 20:24:47 +0900

    Update 4344_평균은 넘겠지.py

--- solve/4344_평균은 넘겠지.py
+++ solve/4344_평균은 넘겠지.py
@@ -4,5 +4,7 @@ input = sys.stdin.readline
 c = int(input())
 for _ in range(c):
   l = list(map(int, input().split()))
-  av = (sum(l) - l[0]) % l[0]
-
+  av = sum(l[1:]) / l[0]
+  cnt = sum(1 for score in l[1:] if score > av)
+  answer = cnt / l[0] * 100
+  print(f"{answer:.3f}", '%', sep = '')
