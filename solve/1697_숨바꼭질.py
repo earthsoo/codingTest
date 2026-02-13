bd5dc0ff43f7ae11affa1d1da348088e9b48bf8a
JISOO KANG <128710723+earthsoo@users.noreply.github.com>
2026-02-13 18:18:33 +0900

    Update 1697_숨바꼭질.py

--- solve/1697_숨바꼭질.py
+++ solve/1697_숨바꼭질.py
@@ -2,8 +2,19 @@
 # BFS 사용
 
 from collections import deque
+#수빈:n, 동생: k
 n, k = map(int, input().split())
 
 queue = deque()
 map = [0] * 100001
 queue.append(n)
+
+while queue:
+  cur = queue.popleft()
+  if cur == k:
+    break
+  for i in (cur+1, cur-1, 2*cur):
+    if 0<= i < 100001 and not map[i]:
+      map[i] = map[cur] + 1
+      queue.append(i)
+print (map[k])
