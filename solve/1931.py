d7adf24570f9e181fb6274cee41e6fd88b856db1
JISOO KANG <128710723+earthsoo@users.noreply.github.com>
2026-03-20 10:45:29 +0900

    Update 1931.py

--- solve/1931.py
+++ solve/1931.py
@@ -7,9 +7,13 @@ for _ in range(n):
   t = tuple(map(int, input().split()))
   times.append(t)
 
-times.sort(key=lambda x:x[1],x[0]) #종료시간 우선 고려, 같으면 시작시간 고려
-cnt = 1
-start = times[0,0]
-finish = times[0,1]
-while True:
-  
+times.sort(key=lambda x: (x[1],x[0])) #종료시간 우선 고려, 같으면 시작시간 고려
+cnt = 0
+end_time = 0
+for time in times:
+  if time[0] < end_time:
+    continue
+  else:
+    cnt+=1
+    end_time = time[1]
+print(cnt)
