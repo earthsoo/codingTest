a441ff8f786c50d9e09b437557427f21fa6081cd
JISOO KANG <128710723+earthsoo@users.noreply.github.com>
2026-03-20 11:28:34 +0900

    Update 1003_피보나치 함수.py

--- solve/1003_피보나치 함수.py
+++ solve/1003_피보나치 함수.py
@@ -2,13 +2,18 @@ import sys
 input = sys.stdin.readline
 
 def fibonacci(int n):
-  if n==0 : 
-    return 0;
+  a = 0
+  b = 0
+  if n==0 :
+    a += 1
+    return [a, b];
   elif n==1 :
-    return 1;
+    b += 1
+    return [a, b];
   else:
     return fibonacci(n-1) + fibonacci(n-2)
-    
+
+#입력 받기
 t = int(input())
 for _ in range(t):
   n = int(input())
